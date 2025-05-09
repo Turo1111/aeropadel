8928186db2dd870fa89f875a3202360da60b0101
turo1111 <Matiaszurita1998@gmail.com>
2025-05-09 12:37:45 -0300

    fix image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,6 +54,7 @@ export default function Home() {
 
       if (result.error) {
         setError(result.error)
+        dispatch(setLoading(false))
       } else {
         dispatch(setLoading(false))
         router.push('/dashboard/inicio')
